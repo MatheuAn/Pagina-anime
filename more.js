d08a6c1b05f53a46349b850dac7d1d4af4af5aa5
MatheuAn <130595852+MatheuAn@users.noreply.github.com>
2024-01-08 05:37:23 -0300

    Update more.js

--- more.js
+++ more.js
@@ -1,4 +1,17 @@
-
+// Extrair o ID do vídeo do link fornecido
+    const containerElement = document.querySelector('.youtubeContainer');
+    const videoUrl = containerElement.dataset.linkEmbed;
+    const videoId = videoUrl.split('/').pop().split('?')[0];
+  
+    const youtubeIframe = `
+      <div>
+        <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+      </div>
+    `;
+  
+    // Inserir o conteúdo na div com a classe "youtubeContainer"
+    containerElement.innerHTML = youtubeIframe;
+  
     
     //Title Postagem
    document.addEventListener('DOMContentLoaded', function () {
