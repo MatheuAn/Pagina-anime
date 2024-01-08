c011d2dc139ceff3d91cbe78a9316e5439645831
MatheuAn <130595852+MatheuAn@users.noreply.github.com>
2024-01-08 06:53:51 -0300

    Update pageA.js

--- pageA.js
+++ pageA.js
@@ -1,18 +1,3 @@
-window.onload = function () {
-    // Extrair o ID do vídeo do link fornecido
-    const containerElement = document.querySelector('.youtubeContainer');
-    const videoUrl = containerElement.dataset.linkEmbed;
-    const videoId = videoUrl.split('/').pop().split('?')[0];
-
-    const youtubeIframe = `
-      <div>
-        <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
-      </div>
-    `;
-
-    // Inserir o conteúdo na div com a classe "youtubeContainer"
-    containerElement.innerHTML = youtubeIframe;
-  };
 
 
 // Obtenha os elementos h3 dentro do primeiro span
